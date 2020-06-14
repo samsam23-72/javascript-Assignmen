735262cc839605e8c29c8fee07af197446be3801
samsam23-72 <65030683+samsam23-72@users.noreply.github.com>
2020-06-14 17:52:44 +0500

    Add files via upload

---
+++ Chap 3/app.js
@@ -0,0 +1,17 @@
+//Task 1:
+var age = "I'm 20 years old"
+alert(age)
+
+//Task 2:
+var a = 14;
+alert("You have visited this site" +a+ "time");
+
+//Task 3:
+var birthyear = ("My Birth Year is  1997 <br> ")
+document.write(birthyear)
+
+//Task 4:
+var a = "Aamir has ordered 3 Kurta shalwar & replica of Sana Safinaz on JS Clothing"
+document.write(a)
+
+
---
+++ chap 4/app.js
@@ -0,0 +1,14 @@
+
+//Task 1:
+alert("var x \n var y \n var z");
+
+//  Task 2:
+ var  x =  "legal    are  name1, $name, _123result <br>";
+ var  y =  "illegal  are   1name, @name, ?name <br>";
+ document.write(x,y);
+
+
+
+//Task 3:
+var a = "Variable names can only contain number $ & _ for example: $ my_1st variable \n  Variable must be begin with a letter $ _ . for example $name _name or name \n variable names are case sensitive \n Variable name should not be JS keyword"
+document.write(a);
